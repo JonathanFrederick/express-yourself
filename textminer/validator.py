d340e020161eefa3c10b6ca711bc449347358539
Jonathan Frederick <jrfreder@gmail.com>
2015-09-22 18:10:32 -0400

    added 'r', changed returns in words

--- textminer/validator.py
+++ textminer/validator.py
@@ -4,35 +4,35 @@ import re
 
 
 def binary(st):
-    return re.match('^(0|1)+$', st)
+    return re.match(r'^(0|1)+$', st)
 
 def binary_even(st):
-    return re.match('^(0|1)+0$', st)
+    return re.match(r'^(0|1)+0$', st)
 
 def hex(st):
-    return re.match('^[0-9A-F]+$', st)
+    return re.match(r'^[0-9A-F]+$', st)
 
 def word(st):
-    return re.match('^[0-9a-zA-Z-]*[a-zA-Z]$', st)
+    return re.match(r'^[0-9a-zA-Z-]*[a-zA-Z]$', st)
 
 def words(st, count=None):
-    new_st = re.split('\s+', st)
+    new_st = re.split(r'\s+', st)
     if None in [word(w) for w in new_st]:
-        return False
+        return None
     else:
         if count:
             return count == len(new_st)
         else:
-            return True
+            return new_st
 
 def phone_number(num):
-    return re.match('^\(?[0-9]{3}\)?[. -]?[0-9]{3}[-.]?[0-9]{4}$', num)
+    return re.match(r'^\(?[0-9]{3}\)?[. -]?[0-9]{3}[-.]?[0-9]{4}$', num)
 
 def money(num):
-    return re.match('^\$([0-9]+(,[0-9]{3})*)(\.[0-9]{2})?$', num)
+    return re.match(r'^\$([0-9]+(,[0-9]{3})*)(\.[0-9]{2})?$', num)
 
 def zipcode(num):
-    return re.match('^[0-9]{5}(-[0-9]{4})?$', num)
+    return re.match(r'^[0-9]{5}(-[0-9]{4})?$', num)
 
 def date(num):
-    return re.match('^[0-9]{1,4}[/-][0-9]{1,2}[/-][0-9]{2,4}$', num)
+    return re.match(r'^[0-9]{1,4}[/-][0-9]{1,2}[/-][0-9]{2,4}$', num)
